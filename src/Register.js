d942ef255195d727c5996e73b310476346d2ab56
shriphad <shriphadnike@gmail.com>
2020-12-12 22:18:35 +0530

    Registeration

--- src/Register.js
+++ src/Register.js
@@ -1,13 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Register.css';
+import { Form, Button, Col, InputGroup } from "react-bootstrap"
+import Card from 'react-bootstrap/Card'
 
-class Register extends React.Component {
-    render() {
-        return (
+function Register() {
+
+    return (
+        <div className="container box">
             <>
-                <h3>Register here</h3>
+                <Card className="App">
+                    <Card.Header border="secondary" style={{ textAlign: 'center' }}>
+                        Register
+                        </Card.Header>
+                    <Card.Body>
+                        <div className="Register">
+                            <Form /* onSubmit={this.RegisterForm} */>
+                                <Form.Group controlId="Email">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="text"
+                                        placeholder="Email"
+                                    // onChange={this.HandleUser}
+                                    />
+                                </Form.Group>
+
+
+
+                                <Form.Group controlId="Pass">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="password"
+                                        placeholder="Password"
+                                    //onChange={this.HandlePass}
+                                    />
+                                    <a className="text-muted" style={{ float: 'right', marginRight: '1%' }} href="/register">
+                                        Register?
+                                        </a>
+                                </Form.Group>
+
+                                <Form.Group controlId="ConfirmPass">
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control
+                                        required
+                                        type="password"
+                                        placeholder="Confirm Password"
+                                    //onChange={this.HandleUser}
+                                    />
+                                </Form.Group>
+
+                                {/* {<Form.Group controlId="formBasicCheckbox">
+                                            <Form.Check type="checkbox" label="Check me out" />
+                                        </Form.Group>} */}
+
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                    </Button>
+                            </Form>
+                        </div>
+                    </Card.Body>
+                    <Card.Footer style={{ textAlign: 'center' }}>
+                        <small className="text-muted" >Best of Luck!</small>
+                    </Card.Footer>
+                </Card>
             </>
-        );
-    }
+        </div>
+    );
 }
 
 export default Register;
