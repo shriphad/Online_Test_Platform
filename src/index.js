ace44a48aa55d6b8c578a1ca0c2eb61784a536e4
shriphad <shriphadnike@gmail.com>
2020-12-17 20:48:51 +0530

    route address

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './Components/Routes';
+import Routes from './Routes';
 //import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import Dashboard from './Dashboard';
 
