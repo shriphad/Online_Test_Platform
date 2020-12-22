be22b2d302473205ee237e3a95bbede0363f8d73
saisashank18 <saisashank.kv@gmail.com>
2020-12-22 19:19:26 +0530

    commiting

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -2,66 +2,31 @@ import React from 'react'
 import './Register.css';
 import { Form, Button } from "react-bootstrap"
 import Card from 'react-bootstrap/Card'
+// import teacher from "./teacher.png"
+// import student from "./student.png"
 
 function Register() {
 
     return (
         <div>
-            <>
-                <Card className="RegisterApp">
-                    <Card.Header border="secondary" style={{ textAlign: 'center' }}>
-                        Register
-                        </Card.Header>
+            <Card style={{ width: '60rem' }} style={{textAlign: 'center'}}>
+                <Card className="Teacher" style={{ width: '18rem' }}>
+                    
+                    {/* <Card.Img variant="top" src={teacher} />      */}
                     <Card.Body>
-                        <div className="Register">
-                            <Form /* onSubmit={this.RegisterForm} */>
-                                <Form.Group controlId="Email">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control
-                                        required
-                                        type="text"
-                                        placeholder="Email"
-                                    // onChange={this.HandleUser}
-                                    />
-                                </Form.Group>
-
-
-
-                                <Form.Group controlId="Pass">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control
-                                        required
-                                        type="password"
-                                        placeholder="Password"
-                                    //onChange={this.HandlePass}
-                                    />
-                                </Form.Group>
-
-                                <Form.Group controlId="ConfirmPass">
-                                    <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control
-                                        required
-                                        type="password"
-                                        placeholder="Confirm Password"
-                                    //onChange={this.HandleUser}
-                                    />
-                                </Form.Group>
-
-                                {/* {<Form.Group controlId="formBasicCheckbox">
-                                            <Form.Check type="checkbox" label="Check me out" />
-                                        </Form.Group>} */}
-
-                                <Button variant="primary" type="submit">
-                                    Submit
-                                    </Button>
-                            </Form>
-                        </div>
+                        
+                    <a href="/Teacher"> <Button variant="primary">Register as Teacher</Button></a>
                     </Card.Body>
-                    <Card.Footer style={{ textAlign: 'center' }}>
-                        <small className="text-muted" >Best of Luck!</small>
-                    </Card.Footer>
                 </Card>
-            </>
+                <Card className="Teacher" style={{ width: '18rem' }}>
+                        
+                    {/* <Card.Img variant="top" src={student} /> */}
+                    <Card.Body>
+                        
+                    <a href="/Student"> <Button variant="primary">Register as Student</Button></a>
+                    </Card.Body>
+                </Card>
+            </Card>
         </div>
     );
 }
